740e77a73a77f035ce539ff1a4431c28c5a22f89
mangal9399 <nikkhema@gmail.com>
2022-09-23 02:04:16 +0530

    code fix by mangal

--- prime.js
+++ prime.js
@@ -1,8 +1,21 @@
-function prime(num){
-    for(let i=1;i<=num;i++){
-        if(num%i){
-            return true;
+function prime(number){
+    let factor =0;
+    for(let i=1;i<=number;i++){
+        if(number%i==0){
+            factor++;
+            
         }
     }
+    if(factor ==2){
+        return true;
+    }
     return false;   
+}
+
+let ans=prime(13)
+if(ans==true){
+    console.log("Prime")
+}
+else{
+    console.log("Not Prime")
 }
