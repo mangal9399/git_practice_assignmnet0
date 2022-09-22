736bb27ecf577306e86f9f8aa5339b66078b4110
AbhayKumarVishwakarma <3492akv@gmail.com>
2022-09-23 01:22:42 +0530

    code fix by abhay

--- prime.js
+++ prime.js
@@ -1,8 +1,21 @@
 function prime(num){
-    for(let i=1;i<=num;i++){
-        if(num%i){
-            return true;
+    counter = 0;
+    for (let i=1; i<=num; i++) {
+        if ( num % i == 0) {
+            counter++;
         }
     }
+    if (counter === 2) {
+          return true;
+    }
     return false;   
+}
+
+let answer = prime(17);
+
+if (answer == true) {
+    console.log("prime");
+}
+else {
+    console.log("Not Prime");
 }
